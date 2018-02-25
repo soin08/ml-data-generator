b3dabc4f27949ed73b06e5b0558b6a2ec8fe1379
yanushP <yanvit97@gmail.com>
2018-02-25 18:15:16 +0300

    add point generator

--- generator.py
+++ generator.py
@@ -1,17 +1,31 @@
 from dataclasses import dataclass
 from typing import List
+import math
 import numpy as np
 
 
 @dataclass
-class SetProperties:
+class Properties:
     radius: int = 5
-    num_samples: int = 100
-    num_features: int = 2
+    num_samples: int = 100   #number of points
+    num_features: int = 2    #dimension
     intersects: List[int] = None
     distances: List[int] = None
 
 
+class Object:
+    def __init__(self, properties : Properties):
+        self.prop = properties
+        self.points = np.ndarray(shape=(self.prop.num_samples, self.prop.num_features), dtype=float)
+        self.create_points()
+
+    def create_points(self):
+       for point_id in range(self.prop.num_samples):
+           vector = np.random.uniform(-1, 1, self.prop.num_features)
+           radius = np.random.uniform(0, self.prop.radius)
+           norm = math.sqrt(sum([el * el for el in vector]))
+           self.points[point_id] = [cord / norm * radius for cord in vector]
+
+
 if __name__ == "__main__":
-    circle = SetProperties(num_samples=1234, distances=[1,2,3])
-    print (circle.distances)
+    obj = Object(Properties())
