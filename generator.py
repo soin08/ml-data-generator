061dfc2f7811579c21a619fb7de341551c83fba9
Ilya Soin <ilya.soin.95@gmail.com>
2018-02-25 16:46:37 +0300

    initial commit

---
+++ generator.py
@@ -0,0 +1,15 @@
+from dataclasses import dataclass
+from typing import List
+import numpy as np
+
+
+@dataclass
+class SetProperties:
+    num_classes: int
+    num_samples: int
+    num_features: int
+    intersects: List[int]
+    linearly_separable: List[int]
+    distances: List[int]
+
+
