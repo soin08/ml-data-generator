90361b8012dbdff19d84e5765310c50d73142b2c
Ilya Soin <ilya.soin.95@gmail.com>
2018-02-25 18:48:41 +0300

    add plot method

--- generator.py
+++ generator.py
@@ -1,4 +1,5 @@
 from dataclasses import dataclass
+import matplotlib.pyplot as plt
 from typing import List
 import math
 import numpy as np
@@ -7,7 +8,7 @@ import numpy as np
 @dataclass
 class Properties:
     radius: int = 5
-    num_samples: int = 100   #number of points
+    num_samples: int = 1000   #number of points
     num_features: int = 2    #dimension
     intersects: List[int] = None
     distances: List[int] = None
@@ -20,12 +21,19 @@ class Object:
         self.create_points()
 
     def create_points(self):
-       for point_id in range(self.prop.num_samples):
-           vector = np.random.uniform(-1, 1, self.prop.num_features)
-           radius = np.random.uniform(0, self.prop.radius)
-           norm = math.sqrt(sum([el * el for el in vector]))
-           self.points[point_id] = [cord / norm * radius for cord in vector]
+        for point_id in range(self.prop.num_samples):
+            vector = np.random.uniform(-1, 1, self.prop.num_features)
+            radius = np.random.uniform(0, self.prop.radius)
+            norm = math.sqrt(sum([el * el for el in vector]))
+            self.points[point_id] = [cord / norm * radius for cord in vector]
+
+    def plot(self, feature1: int, feature2: int):
+        plt.figure(figsize=(10, 10))
+        plt.plot(self.points[:, feature1], self.points[:, feature2], 'ro')
+        plt.show()
 
 
 if __name__ == "__main__":
-    obj = Object(Properties())
+    obj = Object(Properties(radius=10, num_samples=int(1e4), num_features=3))
+    obj.plot(0, 2)
+
